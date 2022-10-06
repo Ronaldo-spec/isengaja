9405a241d2951cf79c6e7aff6b21c049fe8a4af3
bedman-spec <ronaldofirmansyah.30@gmail.com>
2022-10-06 09:04:14 +0700

    iseng1

---
+++ resources/views/next.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <style type="text/css">
+		p
+		{
+		}
+		.pertama
+		{
+			margin-top: 50px;
+			margin-left: 380px;
+            font-size: 80px;
+		}
+	</style>
+  </head>
+  <body>
+<p class="pertama">
+	HELLO WORLD
+</p>
+<!-- <center><img align="center" src="/images/x.png"  alt="image" ></center> -->
+<!-- <center><img align="center" src="/images/1.gif"  alt="image" ></center> -->
+<center><video controls>
+    <source src="/images/capek.mp4" type="video/mp4" />
+  </video></center>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tessya Firmansyah</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <style type="text/css">
+		p
+		{
+		}
+		.pertama {
+			margin-top: 50px;
+			margin-left: 350px;
+      font-size: 50px;
+		}
+    .kedua {
+      display: flex;
+      justify-content: center;
+        }
+    .lanjut {
+      margin-top: 10px;
+        }    
+	</style>
+  </head>
+  <body>
+<p class="pertama">
+	Jangan Cemberut Terus, :*
+</p>
+</body>
+<style>
+   img {
+     width: 300px;
+     height: 400px;
+   }
+</style>
+</head>
+<body> <center>
+  <img src="/images/3.jpg"><img src="/images/1.gif"></center>
+</body>
+
+   
+   
+    <p class="lanjut"><center><a href="next.html"><button type="button" class="btn btn-warning">Warning</button></a></center></p>
+</html>
+
+
+
+<!-- p class="kedua">
+<!-- <center><img align="center" src="/images/x.png"  alt="image" ></center> -->
+<!-- <center><img align="center" src="/images/1.gif"  alt="image" ></center>
+<center><video controls>
+    <source src="/images/capek.mp4" type="video/mp4" />
+  </video></center>
+  </p> -->
